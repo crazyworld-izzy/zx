667afaf2d66f4624031f195e519e328ca6f1d709
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-12-26 09:25:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("API_ID"))
-API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", "27360642"))
+API_HASH = getenv("API_HASH", "3159ea7d00a3c266bfbdb45fbf7fe91f")
 
-BOT_TOKEN = getenv("BOT_TOKEN")
-MONGO_DB_URI = getenv("MONGO_DB_URI", None)
+BOT_TOKEN = getenv("BOT_TOKEN", "8120562276:AAHVyD_2E8J45GdsdlQkg3hCUhfWks1sxoY")
+MONGO_DB_URI = getenv("MONGO_DB_URI", "mongodb+srv://Bikash:Bikash@bikash.yl2nhcy.mongodb.net/?retryWrites=true&w=majority")
 
-OWNER_ID = int(getenv("OWNER_ID", 1356469075))
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FallenAssociation")
+OWNER_ID = int(getenv("OWNER_ID", 7078122796)
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/beast_fox_network")
